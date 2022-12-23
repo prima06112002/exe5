834a7f4f15525f10b5466a5c8989b8320c5d6a2c
Prima06112002 <ramadhan06112002@gmail.com>
2022-12-23 16:18:00 +0700

    add list menu

--- exe5/Program.cs
+++ exe5/Program.cs
@@ -69,7 +69,21 @@ namespace exe5
             char ch;
             while (true)
             {
+                try
+                {
+                    Console.WriteLine("Menu");
+                    Console.WriteLine("1. Implement insert operation");
+                    Console.WriteLine("2. Implement delete operation");
+                    Console.WriteLine("3. Display values");
+                    Console.WriteLine("4. Exit");
+                    Console.Write("\nEnter your choice: ");
+                    ch = Convert.ToChar(Console.ReadLine());
+                    Console.WriteLine();
+                    switch (ch)
+                    {
 
+                    }
+                }
             }
         }
     }
